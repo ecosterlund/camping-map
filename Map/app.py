7c4bc1d5e12a9f4816f8919af62d5ae27c6886eb
ecosterlund <erikosterlund@comcast.net>
2021-05-01 12:02:33 -0400

    attempting to connect to firemap database

--- Map/app.py
+++ Map/app.py
@@ -1,21 +1,24 @@
-# 1. import Flask
+# 1. import Flask and dependencies
 from flask import Flask
 from flask import Flask, jsonify
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import numpy as np
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///fire_db.sqlite")
+engine = fire_db.engine.execute("SQL/fire_db.sql")
+
+# engine = create_engine("sqlite:///SQL/fire_db.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 # Save reference to the table
-Passenger = Base.classes.passenger
+Fire = Base.classes
 
 #################################################
 # Flask Setup
@@ -28,37 +31,39 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/story<br/>"
+        f"/jsonify"
     )
-@app.route("/api/v1.0/names")
+@app.route("/story")
 def names():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    """Return a list of all Fire Coordinates"""
+    # Query all Fires
+    results = session.query(Fire.lat, Fire.lng).all()
     session.close()
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-    return jsonify(all_names)
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-    session.close()
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-    return jsonify(all_passengers)
+    fire_coord = list(np.ravel(results))
+    print(fire_coord)
+    return jsonify(fire_coord)
+
+# @app.route("/jsonify")
+# def Fires():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+#     """Return a list of Fire data including the name, age, and sex of each Fire"""
+#     # Query all Fires
+#     results = session.query(Fire.name, Fire.age, Fire.sex).all()
+#     session.close()
+#     # Create a dictionary from the row data and append to a list of all_Fires
+#     all_Fires = []
+#     for name, age, sex in results:
+#         Fire_dict = {}
+#         Fire_dict["name"] = name
+#         Fire_dict["age"] = age
+#         Fire_dict["sex"] = sex
+#         all_Fires.append(Fire_dict)
+#     return jsonify(all_Fires)
 
 if __name__ == "__main__":
     app.run(debug=True)
